f6d64c27cf4155c65d55301f518fa2219c7d89d1
Cara Eppes <ceppes@udel.edu>
2019-03-04 10:29:21 -0500

    Completed collections

--- src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
+++ src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
@@ -1,37 +1,56 @@
 package rocks.zipcode.assessment2.collections;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Use a map to keep track of inventory in a store
  */
 public class Inventory {
+    private List<String> strings;
+    private Map<String, Integer> inventoryMap;
     /**
      * @param strings list of strings to add / remove / fetch from
      */
     public Inventory(List<String> strings) {
-
+        this.strings = strings;
+        inventoryMap = new TreeMap<>();
+        for(String s : strings) {
+            if (!inventoryMap.containsKey(s)) {
+                inventoryMap.put(s, 0);
+            } else {
+                inventoryMap.put(s, inventoryMap.get(s) + 1);
+            }
+        }
     }
 
     /**
      * nullary constructor initializes a new list
      */
     public Inventory() {
-
+        this.strings = new ArrayList<>();
+        this.inventoryMap = new TreeMap<>();
     }
 
     /**
      * @param item - increment the number of this item in stock by 1
      */
     public void addItemToInventory(String item) {
-        return;
+        if (!inventoryMap.containsKey(item)) {
+            inventoryMap.put(item, 1);
+        } else {
+            inventoryMap.put(item, (inventoryMap.get(item) + 1));
+        }
     }
 
     /**
      * @param item - decrement the number of this item in stock by 1
      */
     public void removeItemFromInventory(String item) {
-        return;
+
+        inventoryMap.put(item, inventoryMap.get(item) - 1);
     }
 
     /**
@@ -39,6 +58,11 @@ public class Inventory {
      * @return - return the number of items
      */
     public Integer getItemQuantity(String item) {
-        return null;
+       if(inventoryMap.containsKey(item)){
+           return inventoryMap.get(item);
+       }
+       else{
+           return 0;
+       }
     }
 }
--- src/main/java/rocks/zipcode/assessment2/collections/MonthConversion.java
+++ src/main/java/rocks/zipcode/assessment2/collections/MonthConversion.java
@@ -1,15 +1,21 @@
 package rocks.zipcode.assessment2.collections;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * Use a map to solve
  */
 public class MonthConversion {
+
+    Map<Integer, String> monthMap = new TreeMap<>();
+
     /**
      * @param monthNumber - ordinal of month in the year; i.e. January = 1, February = 2
      * @param monthName - name of month
      */
     public void add(Integer monthNumber, String monthName) {
-
+        monthMap.put(monthNumber, monthName);
     }
 
     /**
@@ -17,15 +23,27 @@ public class MonthConversion {
      * @return the name of the respective month
      */
     public String getName(Integer monthNumber) {
-        throw new NullPointerException();
+        if(isValidNumber(monthNumber)){
+            return monthMap.get(monthNumber);
+        }
+        else{
+            return null;
+        }
     }
 
     /**
      * @param monthName - name of month
      * @return - the ordinal of the month in the year
      */
-    public int getNumber(String monthName) {
-        return (Integer)null;
+    public Integer getNumber(String monthName) {
+           if (isValidMonth(monthName)) {
+               for (Map.Entry<Integer, String> e : monthMap.entrySet()) {
+                   if (e.getValue().equals(monthName)) {
+                       return e.getKey();
+                   }
+               }
+           }
+           return null;
     }
 
     /**
@@ -33,7 +51,7 @@ public class MonthConversion {
      * @return true if the monthNumber is in the keySet
      */
     public Boolean isValidNumber(Integer monthNumber) {
-        return null;
+        return monthMap.keySet().contains(monthNumber);
     }
 
     /**
@@ -41,14 +59,18 @@ public class MonthConversion {
      * @return true if the monthName is in the valueSet
      */
     public Boolean isValidMonth(String monthName) {
-        return null;
+        return monthMap.values().contains(monthName);
     }
 
     /**
      * @return number of entries in this mapping
      */
     public Integer size() {
-        return -1;
+        int size = 0;
+        for(Map.Entry<Integer, String> e : monthMap.entrySet()){
+            size++;
+        }
+        return size;
     }
 
     /**
@@ -56,6 +78,7 @@ public class MonthConversion {
      * @param monthName - name of month
      */
     public void update(Integer monthNumber, String monthName) {
+        monthMap.put(monthNumber, monthName);
 
     }
 }
